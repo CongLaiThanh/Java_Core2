74b43ef44d3ada39fad4b9f554caff0771a19426
unknown <sithachoang@gmail.com>
2025-01-05 16:42:47 +0700

    init project

---
+++ src/CyberSecurity/Helloworld.java
@@ -0,0 +1,10 @@
+package CyberSecurity;
+
+public class Helloworld {
+	public static void main(String[] args) {
+		System.out.println("Hello world with java and Thachs");
+		System.out.println("Hello world with java and Thachs");
+		System.out.println("Hello world with java and Thachs");
+	}
+
+}
